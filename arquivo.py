a2ba058c02fe66c422547ac3b4bebc8925e03d16
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2016-11-15 19:58:09 -0200

    Inserção de um contador

--- arquivo.py
+++ arquivo.py
@@ -1,8 +1,10 @@
-def potencia(numero, indice):
+def potencia(numero, indice, i):
 	
+	print "contador: %i" %i
+
 	if indice == 0:
 		return 1
 	
-	return numero * potencia(numero, indice-1)
+	return numero * potencia(numero, indice-1, i+1)
 
-print potencia(2, 10)
+print potencia(2, 10, 1)
