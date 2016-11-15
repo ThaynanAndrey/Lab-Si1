761ab86d20718a93ec606b31198e4e82d28e2bc8
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2016-11-15 20:00:03 -0200

    Alteração no arquivo

--- arquivo.py
+++ arquivo.py
@@ -3,6 +3,10 @@ def potencia(numero, indice):
 	if indice == 0:
 		return 1
 	
+	print "Ainda não chegou a zero!!"
+	print
+	print
+
 	return numero * potencia(numero, indice-1)
 
 print potencia(2, 10)
